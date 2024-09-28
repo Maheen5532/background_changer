572a37e26928b6027e3d34d313163be6430c6db4
Maheen5532 <rapunzel5578@gmail.com>
2024-09-28 14:38:04 +0500

    backgroundchange

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function changeBackground() {
+document.body.style.background = (`rgb(${Math.floor(Math.random() * 256)},${Math.floor(
+    Math.random() * 256
+  )},${Math.floor(Math.random() * 256)})`
+
+)
+
+}
